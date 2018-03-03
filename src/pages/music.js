804dd3d5688b374c3dd7ab4e0a9fc7250411f5fb
Ethan Bellora <Belloramail@gmail.com>
2018-03-03 10:25:14 -0600

    iosfix?

--- src/pages/music.js
+++ src/pages/music.js
@@ -226,7 +226,7 @@ export default class Music extends Component {
     const album = music[this.state.currentAlbum];
     const song = album.songs[this.state.currentSong];
     const { url, playing, volume, muted, loop, played, loaded, duration, playbackRate, currentView, loadingSong } = this.state;
-    const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+    const iOS = !!window.navigator.platform && /iPad|iPhone|iPod/.test(window.navigator.platform);
     return (
       <div className="music">
         <ReactPlayer
